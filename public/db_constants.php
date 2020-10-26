67ed2acf172ed6ec08fe71e1d40f9cb20f190878
reshetu2031 <69109700+reshetu2031@users.noreply.github.com>
2020-10-26 12:31:17 +0300

    small updates on access control, link from email, help user guide and others

--- public/db_constants.php
+++ public/db_constants.php
@@ -2,6 +2,6 @@
 	           defined('SENTRIFUGO_HOST') || define('SENTRIFUGO_HOST','localhost');
 	           defined('SENTRIFUGO_USERNAME') || define('SENTRIFUGO_USERNAME','root');
 	           defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','');
-	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','Sentrifugo');
+	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','HRIS');
 	           
 	         ?>

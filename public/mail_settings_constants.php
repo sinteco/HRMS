0205fd81e5698dc914af6ab66278645088c1d8ed
sinte <centg2g@gmail.com>
2020-10-26 16:47:04 +0300

    Revert "small updates on access control, link from email, help user guide and others"
    This reverts commit 67ed2acf172ed6ec08fe71e1d40f9cb20f190878.

--- public/mail_settings_constants.php
+++ public/mail_settings_constants.php
@@ -1,8 +1,8 @@
 <?php
-		           defined('MAIL_SMTP') || define('MAIL_SMTP','smtp.gmail.com');
-		           defined('MAIL_USERNAME') || define('MAIL_USERNAME','donotreplaypsiehris@gmail.com');
-		           defined('MAIL_PASSWORD') || define('MAIL_PASSWORD','1234@Pass');
-		           defined('MAIL_PORT') || define('MAIL_PORT','587');
-		           defined('MAIL_AUTH') || define('MAIL_AUTH','true');
-		           defined('MAIL_TLS') || define('MAIL_TLS','tls');
-		         ?>
+	           defined('MAIL_SMTP') || define('MAIL_SMTP','smtp.gmail.com');
+	           defined('MAIL_USERNAME') || define('MAIL_USERNAME','rahel.asted@gmail.com');
+	           defined('MAIL_PASSWORD') || define('MAIL_PASSWORD','raheshastsam1');
+	           defined('MAIL_PORT') || define('MAIL_PORT','587');
+	           defined('MAIL_AUTH') || define('MAIL_AUTH','true');
+	           defined('MAIL_TLS') || define('MAIL_TLS','tls');
+	         ?>
